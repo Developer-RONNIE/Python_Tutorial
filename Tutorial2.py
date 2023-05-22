1604ea2a523e7d6e14e52f4b38af859bc32e0ea3
Alapan Banerjee <ronnieworkspace@gmail.com>
2023-05-22 17:15:20 +0530

    added Tutorial2.py which includes Input, Basic Operators, Conversion of Data Types

---
+++ Tutorial2.py
@@ -0,0 +1,63 @@
+# Taking Input from the user : 
+
+#print(argument)
+print('Hello, What is your Name?')
+name = input()
+print("Hello,",name)
+
+
+############################
+
+
+# Basic Operators
+# '+' is the addition operator 
+# '-' is the substraction operator 
+# '*' is multiplication operator
+# '/' is division operator
+print(64/10)
+
+# using Operators with Data Types
+num1 = 45
+num2 = 3
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+##############################
+
+
+# Some More Operators 
+# '**' is the exponent operator
+# '//' is the integer division which gives us the integer part of the division
+print(64//10)
+# '%' is the modulus Opertor which gives us the remainder
+print (5%2)
+
+
+#using Operators with Data Types
+num3 = (num1**num2)
+print(num3)
+num3 = (num1//num2)
+print(num3)
+num3 = (num1%num2)
+print(num3)
+
+
+##############################
+
+# Conversion of numbers (Implicit / Explicit)
+
+
+print('Pick a number: ')
+num1 = input()
+print('Pick another number: ')
+num2 = input()
+print(type(num2))
+
+SUM = int(num1) + int(num2)
+print(SUM)
+
+
