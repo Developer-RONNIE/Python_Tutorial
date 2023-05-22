37313e3f8629d56d207148e27ce9d832d296fde4
Alapan Banerjee <ronnieworkspace@gmail.com>
2023-05-22 16:30:03 +0530

    added Tutorial1.py which includes Variables & Data Types

---
+++ Tutorial1.py
@@ -0,0 +1,33 @@
+
+#Declaring a Variable in Python
+name = "Ronnie"
+print(name)
+name = 'Raj'
+print (name)
+name1 = 2
+print(name1)
+NAME1 = 3
+print(NAME1)
+#RULES
+# Variable names cannot contain '-'
+# cannot contain underscore/number at the begining of the variable name
+# also cannot contain special symbol such as '#', '*' ,etc.
+# Capitalization matters 
+
+
+# Data Types
+age= 18
+print(age)
+#int ( -5, 0, 5)
+#str ('Ronnie', "Hello", '1', "345")
+#bool True, False
+#float (0.32, 1.232)
+
+#'Hello'
+#123
+#3.22
+#'2'
+#True
+#"3"
+
+
