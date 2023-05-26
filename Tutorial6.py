d299dabdaeaa94a7a5d052ea189efe8e98a3b06b
Alapan Banerjee <ronnieworkspace@gmail.com>
2023-05-26 18:02:01 +0530

    added Tutorial6.py which includes for Loop

---
+++ Tutorial6.py
@@ -0,0 +1,23 @@
+# a for loop is used when we want to iterate over a condition
+# for a certain number of times
+
+# using for loop with 1 increment
+increment1 = ("Increment Of 1")
+for x in range(0, 10,1):#start, stop, increment 
+    print(x)
+print(increment1 + " was printed successfully.")
+
+# using for loop with 2 increment
+increment2 = ("Increment Of 2")
+for y in range(0,10,2):
+    print(y)
+print(increment2 + " was printed successfully..")
+
+
+# using for loop with 5 increment
+increment5= ("Increment Of 5")
+for z in range(0,10,5):
+    print(z)
+print(increment5 + " was printed successfully.")
+
+
