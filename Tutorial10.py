d5aeb534712cbe3b1bcbaac2bd514ff73834c3d0
Alapan Banerjee <ronnieworkspace@gmail.com>
2023-05-27 15:32:25 +0530

    added Tutorial10.py which includes String Methods

---
+++ Tutorial10.py
@@ -0,0 +1,33 @@
+#########  String Methods  #########
+
+# Methods in Python are special functions that belong to specific objects or
+# data types. They allow those objects to perform actions or
+# provide information about themselves.
+
+# Common Methods 
+# .strip(), len(), .lower(), .upper(), .split()
+
+#.strip() in Python removes leading and trailing whitespace characters.
+text = input('Input Something: ')
+print(text)
+print(text.strip())
+
+
+# len() in Python returns the number of elements in a sequence.
+text = input('Input Something: ')
+print(len(text))
+
+# .lower() in Python converts a string to lowercase characters.
+text = input('Input Something: ')
+print(text.lower())
+
+# .upper() in Python converts a string to uppercase characters.
+text = input('Input Something: ')
+print(text.upper())
+
+# .split() in Python splits a string into a list of substrings
+# using the deliminator.
+text = input('Input Something: ')
+print(text.split())
+
+
